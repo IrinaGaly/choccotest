25c4c8cbf49812d6649ef934f413d3d9342261c6
IriGaly <63234900+IrinaGaly@users.noreply.github.com>
2020-07-27 19:38:16 +0200

    Update map.js

--- js/map.js
+++ js/map.js
@@ -12,7 +12,7 @@
      var myPlacemark = new ymaps.Placemark([59.945443227989287, 30.38935262114668], {}, {
         draggable: false,
         iconLayout: 'default#image',
-        iconImageHref: '/icons/marker.svg',
+        iconImageHref: './../../icons/marker.svg',
         iconImageSize: [30, 42],
         iconImageOffset: [-3, -42]
     });
@@ -22,7 +22,7 @@
   var myPlacemarktwo = new ymaps.Placemark([59.911423223563909, 30.50024587065841], {}, {
     draggable: false,
     iconLayout: 'default#image',
-    iconImageHref: '/icons/marker.svg',
+    iconImageHref: './../../icons/marker.svg',
     iconImageSize: [30, 42],
     iconImageOffset: [-3, -42]
 });
@@ -32,7 +32,7 @@ myMap.geoObjects.add(myPlacemarktwo);
 var myPlacemarkthree = new ymaps.Placemark([59.88693161784606, 30.319658102103713], {}, {
     draggable: false,
     iconLayout: 'default#image',
-    iconImageHref: '/icons/marker.svg',
+    iconImageHref: './../../icons/marker.svg',
     iconImageSize: [30, 42],
     iconImageOffset: [-3, -42]
 });
@@ -42,7 +42,7 @@ myMap.geoObjects.add(myPlacemarkthree);
 var myPlacemarkfour = new ymaps.Placemark([59.97033574821672, 30.315194906302924], {}, {
     draggable: false,
     iconLayout: 'default#image',
-    iconImageHref: '/icons/marker.svg',
+    iconImageHref: './../../icons/marker.svg',
     iconImageSize: [30, 42],
     iconImageOffset: [-3, -42]
 });
